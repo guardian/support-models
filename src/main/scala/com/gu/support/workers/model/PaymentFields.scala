2f7d51956bb9d14fcc02d0f32c98ca705f74d56d
rupertbates <rupert.bates@guardian.co.uk>
2018-04-10 13:23:03 +0100

    test

--- src/main/scala/com/gu/support/workers/model/PaymentFields.scala
+++ src/main/scala/com/gu/support/workers/model/PaymentFields.scala
@@ -6,6 +6,8 @@ case class PayPalPaymentFields(baid: String) extends PaymentFields
 
 case class StripePaymentFields(stripeToken: String) extends PaymentFields
 
+
+
 case class DirectDebitPaymentFields(
   accountHolderName: String,
   sortCode: String,
